e48483e3a5863ee18367fb29db2ed2e440344b93
babyboomer53 <edgar.cole@san.rr.com>
2018-06-01 18:08:44 -0700

    Just tidying up a bit

--- resources/js/scroll.js
+++ resources/js/scroll.js
@@ -1,15 +1,12 @@
-$(document).ready(function() {
-  var scrolllink = $(".scroll");
-
-  // Smooth scrolling
-
-  scrolllink.click(function(event) {
-    event.preventDefault();
-    $("body,html").animate(
-      {
-        scrollTop: $(this.hash).offset().top
-      },
-      1000
-    );
-  });
+$(document).ready(function () {
+    var scrolllink = $(".scroll");
+    scrolllink.click(function (event) {
+        event.preventDefault();
+        $("body,html").animate(
+            {
+                scrollTop: $(this.hash).offset().top
+            },
+            1000
+        );
+    });
 });
